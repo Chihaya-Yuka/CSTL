bcea0e0d74899a523f296d804190c27de9845c58
Chihaya-Yuka <me@yuka.living>
2025-02-06 11:28:57 +0800

    C++23: Unreachable function

---
+++ src/unreachable.c
@@ -0,0 +1,5 @@
+#include "unreachable.h"
+
+void unreachable(){
+    return;
+}
---
+++ src/unreachable.h
@@ -0,0 +1,8 @@
+#ifndef UNREACHABLE_H
+#define UNREACHABLE_H
+
+#include <stddef.h>
+
+void unreachable();
+
+#endif // UNREACHABLE_H
